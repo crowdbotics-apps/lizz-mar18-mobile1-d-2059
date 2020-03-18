dddae51f41266eb4296ba922df46bfa6585fb04c
Crowdbotics <team@crowdbotics.com>
2020-03-18 22:45:17 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,10 @@
+from rest_framework import viewsets
+from rest_framework import authentication
+from .serializers import (
+    CustomModel344pmSerializer,
+    CustomTextSerializer,
+    HomePageSerializer,
+)
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -11,7 +18,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import CustomModel344pm, CustomText, HomePage
 
 
 class SignupViewSet(ModelViewSet):
@@ -49,3 +56,12 @@ class HomePageViewSet(ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = [IsAdminUser]
     http_method_names = ["get", "put", "patch"]
+
+
+class CustomModel344pmViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomModel344pmSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = CustomModel344pm.objects.all()
--- backend/lizz_mar18_mobile1_d_2059/wsgi.py
+++ backend/lizz_mar18_mobile1_d_2059/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizz_mar18_mobile1_d_2059.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lizz_mar18_mobile1_d_2059.settings")
 
 application = get_wsgi_application()
